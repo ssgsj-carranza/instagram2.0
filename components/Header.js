8831239dc62039a27d48b24bf661b2f9b0a29fa9
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-02 18:44:36 -0500

    added screen conditional to render out instagram icon when on a small screen and to switch to instagram written out on a large screen

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,18 @@ function Header() {
     return (
         <div>
             <div className="flex justify-between bg-white max-w-6xl">
+                
                 {/* left */}
-            <div className="relative hidden lg:inline-grid h-24 w-24">
+            <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
                 <Image 
                     src='https://links.papareact.com/ocw'
+                    layout='fill'
+                    objectFit='contain'
+                />
+            </div>
+            <div className="relative w-10 h-10  lg:hidden flex-shrink-0 cursor-pointer">
+            <Image 
+                    src='https://links.papareact.com/jjm'
                     layout='fill'
                     objectFit='contain'
                 />
