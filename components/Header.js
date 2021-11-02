0acb8c288a4901545e6053f7c1f8915fc65ef86f
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-02 18:35:46 -0500

    created next config file to white list domain name of image being pulled into header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,20 @@
+import Image from 'next/image';
+
 function Header() {
     return (
         <div>
-            
+            <h1>HEADER</h1>
+            {/* left */}
+            <div>
+                <Image 
+                    src='https://links.papareact.com/ocw'
+                    layout='fill'
+                />
+            </div>
+
+            {/* middle */}
+
+            {/* right */}
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header';
 
 export default function Home() {
   return (
@@ -7,8 +8,8 @@ export default function Home() {
         <title>Instagram-2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Instagram 2.0</h1>
       {/* header */}
+      <Header />
 
       {/* feed */}
 
