6430f3dc25dcaae98be2ee0e72e1f447eefef8e7
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-02 18:26:17 -0500

    created components folder, begining work on header portion

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Instagram-2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Instagram 2.0</h1>
+      {/* header */}
+
+      {/* feed */}
+
+      {/* modal */}
     </div>
-  )
+  );
 }
