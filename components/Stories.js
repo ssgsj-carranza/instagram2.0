71c340fffbd810674f1dadece7f18b7f4f1d341d
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-03 20:59:53 -0500

    added suggestions variable and gave it state to contain suggestions being pulled from faker library, began mapping through each profile pulled from faker, to pull img, and username, under a story component, created story component, continuing work on story

--- components/Stories.js
+++ components/Stories.js
@@ -1,19 +1,27 @@
 import faker from 'faker';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Story from './Story';
 
 function Stories() {
+    const [suggestions, setSuggestions] = useState([]);
     
     useEffect(() => {
         const suggestions = [...Array(20)].map((_, i) => ({
             ...faker.helpers.contextualCard(),
             id: i,
         }));
-        console.log(suggestions);
+        setSuggestions(suggestions);
     }, [])
 
     return (
         <div>
-            
+            {suggestions.map((profile) => (
+                <Story 
+                    key={profile.id}
+                    img={profile.avatar}
+                    username={profile.username}
+                />
+            ))}
         </div>
     )
 }
