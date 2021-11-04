0d02e2b0fde5f742796d3f982f39b20b370061c1
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-03 21:09:12 -0500

    added some styling to story feed, still tweaking it a bit more

--- components/Story.js
+++ components/Story.js
@@ -1,8 +1,10 @@
 function Story({img, username}) {
     return (
-        <div>
-            <img src={img} alt="" />
-            <p>{username}</p>
+        <div className="">
+            <img className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer" 
+                 src={img} 
+                 alt="" />
+            <p className="text-xs w-14 truncate text-center">{username}</p>
         </div>
     )
 }
