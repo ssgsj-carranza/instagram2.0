d20dfc4017d55bb9fb7370ed5d1820b8d0944dbd
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-03 20:01:27 -0500

    created custom utility tailwind class, under styles folder, changed import in app.js to accept gobals file, applied custom class to icond in header

--- components/Header.js
+++ components/Header.js
@@ -38,9 +38,9 @@ function Header() {
             
             {/* right icons*/}
                 <div className="flex items-center justify-end space-x-4">
-                    <HomeIcon className="h-10 w-10"/>
+                    <HomeIcon className="navBtn"/>
                     <MenuIcon className='h-6 md:hidden cursor-pointer'/>
-                    <PaperAirplaneIcon className='h-10 w-10' />
+                    <PaperAirplaneIcon className='navBtn' />
                     {/* <PlusCircleIcon />
                     <UserGroupIcon />
                     <HeartIcon /> */}
