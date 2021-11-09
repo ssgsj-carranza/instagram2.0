cd08c9eb115b04d91ee1986033ac7b40ae1df5c9
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-08 20:07:55 -0600

    created dummy data for now for the posts, rendered it and mapped through each post, working on post to render onto screen

--- components/Posts.js
+++ components/Posts.js
@@ -1,11 +1,38 @@
 import Post from "./Post"
 
+
+const posts = [
+    {
+        id:'1230',
+        username:'laidpidgeon',
+        userImage:'https://links.papareact.com/ocw',
+        img:'https://links.papareact.com/ocw',
+        caption:'First post',
+    },
+    {
+        id:'1230',
+        username:'laidpidgeon',
+        userImage:'https://links.papareact.com/ocw',
+        img:'https://links.papareact.com/ocw',
+        caption:'Second post',
+    },
+];
+
 function Posts() {
     return (
         <div>
-            <Post />
+            {posts.map((post) => (
+                <Post 
+                    key={post.id}
+                    id={post.id}
+                    username={post.username}
+                    userImage={post.userImage}
+                    img={post.img}
+                    caption={post.caption}
+                />
+            ))}
         </div>
-    )
+    );
 }
 
 export default Posts
