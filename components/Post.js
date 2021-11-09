5813429eb2bb736f95d335c5282db6ba105a2eb7
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-08 20:14:56 -0600

    pulled in necessary icons, divided the post components into required divs, rendered out username with dots icon to test out

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,30 @@
-function Post() {
+import {BookmarkIcon, ChatIcon, DotsHorizontalIcon, EmojiHappyIcon, HeartIcon, PaperAirplaneIcon} from '@heroicons/react/outline';
+import {HeartIcon as HeartIconFillled} from '@heroicons/react/solid';
+
+function Post({id, username, userImage, img, caption}) {
     return (
         <div>
-           <h1>post</h1> 
+            {/*header  */}
+            <div>
+                <img src="" alt="" />
+            <p>{username}</p>
+            <DotsHorizontalIcon className="h-5" />
+            </div>
+
+            {/* img */}
+
+
+            {/* buttons */}
+
+
+            {/* caption */}
+
+
+            {/* comments */}
+
+
+            {/* input box */}
+
         </div>
     )
 }
