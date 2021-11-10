8803d5d7fe51c72e2855a4d0000b3dbcdac6ea00
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-09 18:43:48 -0600

    rendered out suggestions, and pulled in profile avatar from faker library, added some styling, continuing work

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -19,7 +19,14 @@ function Suggestions() {
             <div className="flex justify-between text-sm mb-5">
                 <h3 className="text-sm font-bold text-gray-400">Suggested friends</h3>
                 <button className="text-gray-600 font-semibold">See All</button>
-            </div>                        
+            </div>
+            {
+                suggestions.map((profile) => (
+                    <div key={profile.id} className='flex items-center justify-between mt-3'>
+                        <img src={profile.avatar} alt="" />
+                    </div>
+                ))
+            }                        
         </div>
     )
 }
