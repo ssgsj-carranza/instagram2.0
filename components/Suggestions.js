a69296e4f97a7ea37723b31f321f8fc186024a80
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-09 18:41:07 -0600

    added header for suggested friends and see all button, added styling to those sections

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -15,8 +15,11 @@ function Suggestions() {
     },[]);
 
     return (
-        <div>
-                            
+        <div className="mt-4 ml-10">
+            <div className="flex justify-between text-sm mb-5">
+                <h3 className="text-sm font-bold text-gray-400">Suggested friends</h3>
+                <button className="text-gray-600 font-semibold">See All</button>
+            </div>                        
         </div>
     )
 }
