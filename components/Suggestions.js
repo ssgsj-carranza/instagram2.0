02bed1796ef5952919d642c12e47ae8c7c4c14ad
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-09 18:37:10 -0600

    used the same method to populate fake stories from faker, to pull dummy suggestions, using useState and useEffect

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react"
+import faker from 'faker';
+
 function Suggestions() {
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        const suggestions = [...Array(5)].map((_, i) => (
+            {
+               ...faker.helpers.contextualCard(),
+               id: i, 
+            }
+        ));
+        setSuggestions(suggestions);
+    },[]);
+
     return (
         <div>
-            
+                            
         </div>
     )
 }
