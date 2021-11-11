5556f541a3ff018c8bdf7143da8b0d80c1612a54
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-10 21:01:45 -0600

    added callback url so user is redirected to home page after authentication

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -12,7 +12,7 @@ function signIn({providers}) {
                             {Object.values(providers).map((provider) => (
                                 <div key={provider.name}>
                                     <button className="p-3 bg-blue-500 rounded-lg text-white"
-                                            onClick={() => SignIntoProvider(provider.id)}
+                                            onClick={() => SignIntoProvider(provider.id, {callbackUrl: '/'})}
                                     >
                                         Sign in with {provider.name}
                                     </button>
