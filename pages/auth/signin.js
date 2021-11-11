7fa6b9d9ca52ff3e16d235a1a59a390a8b088a2e
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-10 20:18:43 -0600

    set up server side props function, to prep the page, set up sign in rendering based on signin documentation from next-auth, solving issue whereproviders is not being serialized on server side props function

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,28 @@
-function signIn() {
+import {getProviders, signIn as SignIntoProvider} from 'next-auth/react';
+
+//Runs on browser
+function signIn({providers}) {
     return (
-        <div>
-            <h1>sign in page</h1>        
-        </div>
-    )
+            <>
+                {Object.values(providers).map((provider) => (
+                    <div key={provider.name}>
+                        <button onClick={() => signIntoProvider(provider.id)}>Sign in with {provider.name}</button>
+                    </div>
+                ))}
+            </>        
+        
+    );
+}
+
+//Server side render
+export async function getServerSideProps() {
+    const providers = getProviders();
+
+    return {
+        props: {
+            providers
+        }
+    }
 }
 
 export default signIn;
