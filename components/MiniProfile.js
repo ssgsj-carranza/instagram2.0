7a89842c0f52f1f7e6b6cedf7d5e420ed81e70f6
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-11 20:24:24 -0600

    rendered out user image into mini profile component

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -6,14 +6,14 @@ function MiniProfile() {
 
     return (
         <div className="flex items-center justify-between mt-14 ml-10">
-            <img src="https://links.papareact.com/ocw" alt="" className='rounded-full border p-[2px] w-16 h-16' />
+            <img src={session.user.image} alt="" className='rounded-full border p-[2px] w-16 h-16' />
             
             <div className="flex-1 mx-4">
-                <h2 className="font-bold">laidpidgeon</h2>
+                <h2 className="font-bold">{session?.user?.username}</h2>
                 <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
             </div>
             
-            <button className="text-blue-400 text-sm font-semibold">Sign Out</button>        
+            <button onClick={signOut} className="text-blue-400 text-sm font-semibold">Sign Out</button>        
         </div>
     );
 }
