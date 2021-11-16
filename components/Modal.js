3e3dfc49bf71040b0ff11851a207bdc3dcac6031
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-15 20:54:25 -0600

    added transition attributes to modal display

--- components/Modal.js
+++ components/Modal.js
@@ -13,7 +13,17 @@ function Modal() {
                 className='fixed z-10 inset-0 overscroll-y-auto'
                 onClose={setOpen}
             >
-                
+                <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 pb-20 text-center sm:block sm:p-0'>
+                    <Transition.Child
+                        as={Fragment}
+                        enter='ease-out duration 300'
+                        enterFrom='opacity-0'
+                        enterTo='opacity-100'
+                        leave='ease-in duration-200'
+                        leaveFrom='opacity-100'
+                        leaveTo='opacity-0'
+                    ></Transition.Child>
+                </div>        
             </Dialog>
         </Transition.Root>
     )
