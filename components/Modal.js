aad5fa1eb9265c0850d205c9e2ef77c536a95183
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-17 20:08:39 -0600

    added a button to modal and styling to it, I still yet have to implement upload function

--- components/Modal.js
+++ components/Modal.js
@@ -40,7 +40,17 @@ function Modal() {
                         leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
                     >
                         <div className='inline-block align-bottom bg-white rounded-xl px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'>
-                            <h1>hello</h1>    
+                            <div>
+                                <div>
+                                    <button
+                                        type='button'
+                                        className='inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 
+                                        focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300'
+                                    >
+                                        Upload Post
+                                    </button>
+                                </div>
+                            </div>    
                         </div>
                     </Transition.Child>    
                 </div>        
