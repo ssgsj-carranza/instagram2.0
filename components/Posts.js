0f25882e0dd1272e78d6176fbc43d6b135d0dc4b
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-22 21:00:01 -0600

    corrected render errors on posts, image is now rendering on front end with corresponding caption

--- components/Posts.js
+++ components/Posts.js
@@ -8,25 +8,28 @@ function Posts() {
     const [posts, setPosts] = useState([]);
 
     //Every time value on the back end changes, it updates react state with the latest docs
-    useEffect(() => 
-        onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
-            setPosts(snapshot.docs);
-        }), [db]);
-        //never going to attach more than one listener
-        console.log(posts);
+    useEffect(
+        () => 
+            onSnapshot(
+                query(collection(db, 'posts'), orderBy('timestamp', 'desc')), 
+                (snapshot) => {
+                    setPosts(snapshot.docs);
+            }), [db]);
+            //never going to attach more than one listener
+            console.log(posts);
 
     return (
         <div>
-            {/* {posts.map((post) => (
+            {posts.map((post) => (
                 <Post 
                     key={post.id}
                     id={post.id}
                     username={post.data().username}
-                    userImage={post.data().userImage}
-                    img={post.data().img}
+                    userImage={post.data().profileImg}
+                    img={post.data().image}
                     caption={post.data().caption}
                 />
-            ))} */}
+            ))}
         </div>
     );
 }
