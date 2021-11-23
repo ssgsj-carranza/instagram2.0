666214408e2cedcf334f134e6abecfbc55ee18a9
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-22 21:05:42 -0600

    added session conditional to only render like and comment buttons as well as input box if the user is signed in

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,10 @@
 import {BookmarkIcon, ChatIcon, DotsHorizontalIcon, EmojiHappyIcon, HeartIcon, PaperAirplaneIcon} from '@heroicons/react/outline';
 import {HeartIcon as HeartIconFillled} from '@heroicons/react/solid';
+import { useSession } from 'next-auth/react';
 
 function Post({id, username, userImage, img, caption}) {
+    const {data: session} = useSession();
+
     return (
         <div className="bg-white my-7 border rounded-sm">
             {/*header  */}
@@ -15,7 +18,8 @@ function Post({id, username, userImage, img, caption}) {
             <img src={img} alt="" className="object-cover w-full" />
 
             {/* buttons */}
-            <div className="flex justify-between px-4 pt-4">
+            {session && (
+                <div className="flex justify-between px-4 pt-4">
                 <div className='flex space-x-4'>
                     <HeartIcon className='btn' />
                     <ChatIcon className='btn' />
@@ -23,8 +27,8 @@ function Post({id, username, userImage, img, caption}) {
                 </div>
                 <BookmarkIcon className='btn' />
             </div>
-
-
+            )}
+            
             {/* caption */}
             <p className='p-5 truncate'>
                 <span className='font-bold mr-1'>{username} </span>
@@ -35,11 +39,13 @@ function Post({id, username, userImage, img, caption}) {
 
 
             {/* input box */}
-            <form className='flex items-center p-4'>
+            {session && (
+                <form className='flex items-center p-4'>
                 <EmojiHappyIcon className='h-7' />
                 <input type="text" placeholder='Add a comment...' className='border-none flex-1 focus:ring-0 outline-none' />
                 <button className='font-semibold text-blue-400'>Post</button>
             </form>
+            )}
         </div>
     )
 }
