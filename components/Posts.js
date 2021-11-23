02aaa1ed62614846151b41f29bd77ac6188a891e
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-22 20:51:01 -0600

    deleted dummy posts data, cleaned up use effect, working on posts mapping

--- components/Posts.js
+++ components/Posts.js
@@ -4,36 +4,15 @@ import { db } from "../firebase";
 import Post from "./Post"
 
 
-const posts = [
-    {
-        id:'1230',
-        username:'laidpidgeon',
-        userImage:'https://links.papareact.com/ocw',
-        img:'https://links.papareact.com/ocw',
-        caption:'First post',
-    },
-    {
-        id:'1230',
-        username:'laidpidgeon',
-        userImage:'https://links.papareact.com/ocw',
-        img:'https://links.papareact.com/ocw',
-        caption:'Second post',
-    },
-];
-
 function Posts() {
     const [posts, setPosts] = useState([]);
 
     //Every time value on the back end changes, it updates react state with the latest docs
-    useEffect(() => {
-        const unsuscribe = onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+    useEffect(() => 
+        onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
             setPosts(snapshot.docs);
-        });
+        }), [db]);
         //never going to attach more than one listener
-        return () => {
-            unsuscribe();
-        }
-    },[])
 
     return (
         <div>
