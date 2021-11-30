ae8b18e98604d849390b7b0fec0d7c51d04ed3b1
Jorge Carranza <ssgsj.carranza@gmail.com>
2021-11-29 21:11:15 -0600

    begining to render out comments only if there are comments on the post, and adding styling as I go

--- components/Post.js
+++ components/Post.js
@@ -67,7 +67,15 @@ function Post({id, username, userImage, img, caption}) {
             </p>
 
             {/* comments */}
-
+            {comments.length > 0 && (
+                <div className='ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin'>
+                    {comments.map((comment) => (
+                        <div key={comment.id} className='flex items-center space-x-2 mb-3'>
+                            <img className='' src={comment.data().image} alt=''/>
+                        </div>
+                    ))}
+                </div>
+            )}
 
             {/* input box */}
             {session && (
